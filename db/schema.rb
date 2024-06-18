f244160ef0258a9cca3021ae18c2fa45fff1c913
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-18 11:22:34 +1000

    chore: Update database configuration to use PostgreSQL instead of SQLite3

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_17_135205) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.decimal "balance"
@@ -32,7 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_135205) do
   end
 
   create_table "transactions", force: :cascade do |t|
-    t.integer "account_id", null: false
+    t.bigint "account_id", null: false
     t.decimal "amount"
     t.datetime "timestamp"
     t.datetime "created_at", null: false
@@ -49,7 +52,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_135205) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "transactions", "accounts"
