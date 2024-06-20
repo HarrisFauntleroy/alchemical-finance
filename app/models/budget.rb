05f6c4dd2870c81a8f3862f02120726d860393d5
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-20 23:23:17 +1000

    chore: Add user association to Budget model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Budget < ApplicationRecord
+  belongs_to :user
+
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :category, presence: true
