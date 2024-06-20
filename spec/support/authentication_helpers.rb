29c373fd64d122f83488b50ee4f65b47703453d9
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-20 23:24:11 +1000

    chore: Refactor sign up and sign in forms in users_spec.rb and authentication_helpers.rb

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -2,23 +2,40 @@
 
 module AuthenticationHelpers
   def sign_up(user)
-    visit new_user_registration_path
-    fill_in 'user_email', with: user.email
-    fill_in 'user_username', with: user.username
-    fill_in 'user_password', with: user.password
-    fill_in 'user_password_confirmation', with: user.password
-    click_button 'Sign up'
+    navigate_to_sign_up
+    within('#new_user') do
+      fill_in 'user_email', with: user.email
+      fill_in 'user_username', with: user.username
+      fill_in 'user_password', with: user.password
+      fill_in 'user_password_confirmation', with: user.password
+      click_on 'Sign up'
+    end
   end
 
   def sign_in(user)
-    visit new_user_session_path
-    fill_in 'user_email', with: user.email
-    fill_in 'user_password', with: user.password
-    click_button 'Sign in'
+    navigate_to_sign_in
+    within('#new_user') do
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      click_on 'Sign in'
+    end
   end
 
   def sign_out
-    find_by_id('user-profile').click
-    click_button 'Sign out'
+    visit root_path
+    find('#user-profile').click
+    click_on 'Sign out'
+  end
+
+  private
+
+  def navigate_to_sign_up
+    visit root_path
+    within('#navigation') { click_on 'Sign up' }
+  end
+
+  def navigate_to_sign_in
+    visit root_path
+    within('#navigation') { click_on 'Sign in' }
   end
 end
