7617183b10f36fcb6f694caf3c353112e5dd73c2
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-20 23:24:16 +1000

    chore: Update budget factory with additional attributes

--- spec/factories/budgets.rb
+++ spec/factories/budgets.rb
@@ -2,8 +2,15 @@
 
 FactoryBot.define do
   factory :budget do
-    name { 'MyString' }
+    name { 'Test Budget' }
+
+    user
+
     amount { '9.99' }
+    category { 'Test Category' }
+    currency { 'BTC' }
+    frequency { 'Monthly' }
+
     start_date { '2024-06-16' }
     end_date { '2024-06-16' }
   end
