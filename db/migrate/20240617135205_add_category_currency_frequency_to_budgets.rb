3b618ccefdfff7bd6f4ce52f66d9ee434e7da43c
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-20 23:43:08 +1000

    chore: Update RuboCop and Brakeman configurations to exclude config files and improve code quality checks

--- db/migrate/20240617135205_add_category_currency_frequency_to_budgets.rb
+++ db/migrate/20240617135205_add_category_currency_frequency_to_budgets.rb
@@ -2,8 +2,10 @@
 
 class AddCategoryCurrencyFrequencyToBudgets < ActiveRecord::Migration[7.1]
   def change
-    add_column :budgets, :category, :string
-    add_column :budgets, :currency, :string
-    add_column :budgets, :frequency, :string
+    change_table :budgets, bulk: true do |t|
+      t.string :category
+      t.string :currency
+      t.string :frequency
+    end
   end
 end
--- db/migrate/20240620130502_add_user_to_budgets.rb
+++ db/migrate/20240620130502_add_user_to_budgets.rb
@@ -2,6 +2,6 @@
 
 class AddUserToBudgets < ActiveRecord::Migration[7.1]
   def change
-    add_reference :budgets, :user, null: false, foreign_key: true
+    add_reference :budgets, :user, null: false, foreign_key: true, default: 0
   end
 end
