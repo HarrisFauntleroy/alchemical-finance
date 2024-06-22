886e10a9c754a97619ef39cf54b906c9b1b277d2
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-22 16:50:24 +1000

    chore: Refactor budget calculation service to use a hash for frequency multipliers

--- app/services/budget_calculation_service.rb
+++ app/services/budget_calculation_service.rb
@@ -2,10 +2,18 @@
 
 class BudgetCalculationService
   FREQUENCY_MULTIPLIERS = {
-    'Daily' => 30,       # Approximate days in a month
-    'Weekly' => 4.33,    # Approximate weeks in a month
-    'Monthly' => 1,
-    'Yearly' => 1.0 / 12 # Convert yearly to monthly
+    'Daily' => { 'daily' => 1, 'weekly' => 7, 'fortnightly' => 14, 'monthly' => 30, 'quarterly' => 90,
+                 'annually' => 365 },
+    'Weekly' => { 'daily' => 1.0 / 7, 'weekly' => 1, 'fortnightly' => 2, 'monthly' => 4, 'quarterly' => 12,
+                  'annually' => 52 },
+    'Fortnightly' => { 'daily' => 1.0 / 14, 'weekly' => 0.5, 'fortnightly' => 1, 'monthly' => 2, 'quarterly' => 6,
+                       'annually' => 26 },
+    'Monthly' => { 'daily' => 1.0 / 30, 'weekly' => 1.0 / 4, 'fortnightly' => 0.5, 'monthly' => 1, 'quarterly' => 3,
+                   'annually' => 12 },
+    'Quarterly' => { 'daily' => 1.0 / 90, 'weekly' => 1.0 / 12, 'fortnightly' => 1.0 / 6, 'monthly' => 1.0 / 3,
+                     'quarterly' => 1, 'annually' => 4 },
+    'Annually' => { 'daily' => 1.0 / 365, 'weekly' => 1.0 / 52, 'fortnightly' => 1.0 / 26, 'monthly' => 1.0 / 12,
+                    'quarterly' => 1.0 / 4, 'annually' => 1 }
   }.freeze
 
   def initialize(budget)
@@ -13,11 +21,7 @@ class BudgetCalculationService
   end
 
   def amount_for_view(view)
-    multiplier = FREQUENCY_MULTIPLIERS[@budget.frequency] || 0
-    case view
-    when 'monthly'
-      # Add more views if necessary
-    end
+    multiplier = FREQUENCY_MULTIPLIERS[@budget.frequency][view] || 0
     @budget.amount * multiplier
   end
 end
