5a41cda12f5cbfad13f6bd37c7d99d2ab8283f15
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-23 20:40:32 +1000

    chore: Update budgets table to include user reference and amount precision

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_130502) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_23_101648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,15 +24,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_130502) do
 
   create_table "budgets", force: :cascade do |t|
     t.string "name"
-    t.decimal "amount"
+    t.decimal "amount", precision: 10, scale: 2
     t.date "start_date"
     t.date "end_date"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
     t.string "currency"
     t.string "frequency"
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
@@ -51,11 +51,27 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_130502) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  add_foreign_key "budgets", "users"
+  add_foreign_key "budgets", "users", on_delete: :cascade
   add_foreign_key "transactions", "accounts"
 end
