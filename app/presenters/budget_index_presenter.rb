20279767b9e4159caa081f1b95c7c72da41eda7d
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2024-06-23 20:40:47 +1000

    chore: Update BudgetIndexPresenter to include budget_frequency parameter

--- app/presenters/budget_index_presenter.rb
+++ app/presenters/budget_index_presenter.rb
@@ -3,9 +3,9 @@
 class BudgetIndexPresenter
   attr_reader :total_amount, :remaining_amount, :category_distribution, :income, :budgets, :pagy
 
-  def initialize(user, view, budgets, pagy)
+  def initialize(user, budget_frequency, budgets, pagy)
     @user = user
-    @view = view
+    @budget_frequency = budget_frequency
     @budgets = budgets
     @pagy = pagy
     calculate_summary
@@ -18,7 +18,7 @@ class BudgetIndexPresenter
   private
 
   def calculate_summary
-    summary_service = BudgetSummaryService.new(@user, @view)
+    summary_service = BudgetSummaryService.new(@user, @budget_frequency)
     @category_distribution = summary_service.category_distribution
     @total_amount = summary_service.total_amount
     @remaining_amount = summary_service.remaining_amount
